007d1fb4da4941db0193536b8026cef097a0f63b
Sulyvan Dollin <dollin.sulyvan@gmail.com>
2019-08-06 14:38:29 +0200

    Add the home menu

--- stylesheet.py
+++ stylesheet.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 def home():
     return {
-        'backgroundColor': '#F51212',
-        'height': '100px',
-        'width':'20%',
-        'display':'inline-block'
+        'backgroundColor': '#FFFFFF',
+        #'height': '100px',
+        'width':'80px',
+        'display':'inline-block',
+        'text-align':'center',
+        'padding': '50px'
     }
 
 def header():
     return {
         'backgroundColor': '#3f51b5',
         'color': '#050D36',
-        'height': '100px',
-        'width':'80%',
-        'display':'inline-block'
+        #'height': '100px',
+        'width':'1600px',
+        'display':'inline-block',
+        'padding': '50px'
         
     }
-
 
 def title():
     return {
         'color': '#050D36',
-        'fontSize': 80
+        'fontSize': 80,
+        'font-family': 'Bookman, URW Bookman L, serif',
     }
 
