8461bc69c5b0016ab84da8de7dd05b90a209cc9b
Sulyvan Dollin <dollin.sulyvan@gmail.com>
2019-08-21 14:28:13 +0200

    Changing the place of the tooltip

--- layout.py
+++ layout.py
@@ -116,8 +116,9 @@ def mainframe():
 
         html.Br(),
         html.Br(),
-        dbc.Tooltip("Be careful, you are limited to a file of less than 100 MB. For heavier input file please install bioconvert",
-                    target="upload_file",style={ "color":"red"}),
+        dbc.Tooltip(children=[html.P("Be careful, you are limited"),html.P("to a file of less than 100 MB."),
+                    html.P(" For heavier input file"), html.P("please install bioconvert")],
+                    target="upload_file",style={ "color":"red", 'border':'1px dotted black', 'background-color': 'yellow','text-align':'center'}),
 
 
         # Div for second step
